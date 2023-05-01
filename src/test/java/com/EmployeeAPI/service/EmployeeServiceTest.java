de4c942d040bbc4a6178d1b4bf005ba39ff99adc
Tyler Stendara <tstendara@gmail.com>
2023-05-01 00:37:24 -0500

    refactor service test to use junit 5 features and improve test coverage

--- src/test/java/com/EmployeeAPI/service/EmployeeServiceTest.java
+++ src/test/java/com/EmployeeAPI/service/EmployeeServiceTest.java
@@ -1,83 +1,176 @@
 package com.EmployeeAPI.service;
 
 import com.EmployeeAPI.dto.Employee;
+import com.EmployeeAPI.exceptions.EmployeeControllerExceptionHandler.*;
 import com.EmployeeAPI.repository.EmployeeRepository;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 @RunWith(SpringRunner.class)
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+@SpringBootTest
 public class EmployeeServiceTest {
 
     @Mock
     private EmployeeRepository employeeRepository;
 
+    private EmployeeService employeeService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        employeeService = new EmployeeService(employeeRepository);
+    }
+
     @Test
+    @DisplayName("Get all employees")
     void testGetAllEmployees() {
         List<Employee> employees = new ArrayList<>();
-        employees.add(new Employee("John", "sre"));
-        employees.add(new Employee("Jane", "swe"));
-        when(employeeRepository.findAll()).thenReturn(employees);
+        employees.add(new Employee(1L, "John", "Doe", "Developer"));
+        employees.add(new Employee(2L, "Jane", "Doe", "Manager"));
 
-        EmployeeService employeeService = new EmployeeService();
-        employeeService.setEmployeeRepository(employeeRepository);
+        when(employeeRepository.findAll()).thenReturn(employees);
 
         List<Employee> result = employeeService.getAllEmployees();
 
-        Assertions.assertEquals(employees, result);
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("John", result.get(0).getFirstName());
+        assertEquals("Doe", result.get(0).getLastName());
+        assertEquals("Developer", result.get(0).getJobRole());
+        assertEquals("Jane", result.get(1).getFirstName());
+        assertEquals("Doe", result.get(1).getLastName());
+        assertEquals("Manager", result.get(1).getJobRole());
+
+        verify(employeeRepository, times(1)).findAll();
+        verifyNoMoreInteractions(employeeRepository);
     }
 
     @Test
+    @DisplayName("Get employee by id")
     void testGetEmployeeById() {
-        Employee employee = new Employee("John", "sre");
-        when(employeeRepository.findById(1L)).thenReturn(Optional.of(employee));
+        long id = 1L;
+        Employee employee = new Employee(id, "John", "Doe", "Developer");
 
-        EmployeeService employeeService = new EmployeeService();
-        employeeService.setEmployeeRepository(employeeRepository);
+        when(employeeRepository.findById(id)).thenReturn(Optional.of(employee));
 
-        Employee result = employeeService.getEmployeeById(1L);
+        Employee result = employeeService.getEmployeeById(id);
 
-        Assertions.assertEquals(employee, result);
+        assertNotNull(result);
+        assertEquals("John", result.getFirstName());
+        assertEquals("Doe", result.getLastName());
+        assertEquals("Developer", result.getJobRole());
+
+        verify(employeeRepository, times(1)).findById(id);
+        verifyNoMoreInteractions(employeeRepository);
     }
 
     @Test
-    void testAddEmployee() {
-        Employee employee = new Employee("John", "sre");
-        when(employeeRepository.save(employee)).thenReturn(employee);
+    @DisplayName("Get employee by id - EmployeeNotFoundException")
+    void testGetEmployeeByIdEmployeeNotFoundException() {
+        long id = 1L;
 
-        EmployeeService employeeService = new EmployeeService();
-        employeeService.setEmployeeRepository(employeeRepository);
+        when(employeeRepository.findById(id)).thenReturn(Optional.empty());
+
+        assertThrows(EmployeeNotFoundException.class, () -> employeeService.getEmployeeById(id));
+
+        verify(employeeRepository, times(1)).findById(id);
+        verifyNoMoreInteractions(employeeRepository);
+    }
+
+    @Test
+    @DisplayName("Add employee")
+    void testAddEmployee() {
+        Employee employee = new Employee(null, "John", "Doe", "Developer");
+
+        when(employeeRepository.save(employee)).thenReturn(employee);
 
         Employee result = employeeService.addEmployee(employee);
 
-        Assertions.assertEquals(employee, result);
+        assertNotNull(result);
+        assertEquals("John", result.getFirstName());
+        assertEquals("Doe", result.getLastName());
+        assertEquals("Developer", result.getJobRole());
+
+        verify(employeeRepository, times(1)).save(employee);
+        verifyNoMoreInteractions(employeeRepository);
     }
 
     @Test
+    @DisplayName("Add employee - InvalidEmployeeDataException")
+    void testAddEmployeeInvalidEmployeeDataException() {
+        Employee employee = new Employee();
+        assertThrows(InvalidEmployeeDataException.class, () -> employeeService.addEmployee(employee));
+    }
+
+    @Test
+    @DisplayName("Update employee")
     void testUpdateEmployee() {
-        Employee existingEmployee = new Employee("John", "sre");
-        Employee updatedEmployee = new Employee("Jane", "swe");
-        when(employeeRepository.findById(1L)).thenReturn(Optional.of(existingEmployee));
-        when(employeeRepository.save(existingEmployee)).thenReturn(updatedEmployee);
+        long id = 1L;
+        Employee existingEmployee = new Employee(id, "John", "Doe", "Developer");
+        Employee updatedEmployee = new Employee(id, "Jane", "Doe", "Manager");
 
-        EmployeeService employeeService = new EmployeeService();
-        employeeService.setEmployeeRepository(employeeRepository);
+        when(employeeRepository.findById(id)).thenReturn(Optional.of(existingEmployee));
+        when(employeeRepository.save(updatedEmployee)).thenReturn(updatedEmployee);
 
-        Employee result = employeeService.updateEmployee(1L, updatedEmployee);
+        Employee result = employeeService.updateEmployee(id, updatedEmployee);
 
-        Assertions.assertEquals(updatedEmployee, result);
+        assertNotNull(result);
+        assertEquals("Jane", result.getFirstName());
+        assertEquals("Doe", result.getLastName());
+        assertEquals("Manager", result.getJobRole());
+
+        verify(employeeRepository, times(1)).findById(id);
+        verify(employeeRepository, times(1)).save(updatedEmployee);
+        verifyNoMoreInteractions(employeeRepository);
+    }
+
+    @Test
+    @DisplayName("Update employee - EmployeeNotFoundException")
+    void testUpdateEmployeeEmployeeNotFoundException() {
+        assertThrows(EmployeeNotFoundException.class, () -> employeeService.updateEmployee(1, new Employee()));
+    }
+
+    @Test
+    @DisplayName("Delete employee")
+    void testDeleteEmployee() {
+        long id = 1L;
+        Employee employee = new Employee(id, "John", "Doe", "Manager");
+
+        when(employeeRepository.existsById(id)).thenReturn(true);
+        when(employeeRepository.save(employee)).thenReturn(employee);
+
+        Employee addedEmployee = employeeService.addEmployee(employee);
+
+        boolean result = employeeService.deleteEmployee(addedEmployee.getId());
+
+        assertTrue(result);
+        verify(employeeRepository, times(1)).existsById(addedEmployee.getId());
+        verify(employeeRepository, times(1)).deleteById(addedEmployee.getId());
+        verifyNoMoreInteractions(employeeRepository);
+
+        assertThrows(EmployeeNotFoundException.class, () -> employeeService.getEmployeeById(addedEmployee.getId()));
+    }
+
+    @Test
+    @DisplayName("Delete employee - EmployeeNotFoundException")
+    void testDeleteEmployeeEmployeeNotFoundException() {
+        assertThrows(EmployeeNotFoundException.class, () -> employeeService.deleteEmployee(1));
     }
 }
-
